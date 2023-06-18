3e0bf6ccc540de4d8e876b8b57991746457a53da
AKS2044 <thefan49@gmail.com>
2023-06-18 22:35:59 +0300

    feat: cleared the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [track, setTrack] = useState<HTMLAudioElement>();
+  const inputFileRef = useRef<HTMLInputElement>(null);
+
+  if (track) track?.play();
+
+  const handleChangeFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      const file = e.target.files![0];
+      //let audio = new Audio(file.);
+      console.log(e);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          handleChangeFile(e)
+        }
+        type="file"
+        accept=".mp3,audio/*"
+        ref={inputFileRef}
+        hidden
+      />
+      <button onClick={() => inputFileRef.current?.click()} className="button">
+        Choose mp3 file
+      </button>
     </div>
   );
 }
